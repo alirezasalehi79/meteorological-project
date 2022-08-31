3c9e1c448c60a919c274c1f83bfcf9f4c04e8b44
alireza <alirezawmds1379@gmail.com>
2022-08-30 22:20:05 -0700

    meteorological project initialized

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+
+api_key="908a628a1815decea0de4a816a516291"
+base_url="http://api.openweathermap.org/data/2.5/weather?q="
+cityName=input("Enter city name:")
+url=base_url + cityName + "&appid=" + api_key
+#print(url)
+
+result = requests.get(url)
+data = result.json()
+#print(data)
+if data["cod"] == "404":
+    print("city not found")
+else:
+    a=data["main"]
+    #print(a)
+
+    t=a["temp"]
+    tc = t - 273.15
+    print(str(round(tc,2)) + " c")
+    #print(int(tc))
+
+    p=a["pressure"]
+    print(str(p) + " hpa")
+
+    h=a["humidity"]
+    print(str(h)+" %")
+
+    w=data["weather"]
+    #print(w)
+    d=w[0]["description"]
+    print(d)
+
+
+
+
+
+
---
+++ practice2.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+import requests
+
+window = tk.Tk()
+window.geometry("300x300")
+
+lblNotFound = tk.Label(window)
+lblTemp = tk.Label(window)
+lblpressure = tk.Label(window)
+lblhumidity = tk.Label(window)
+lbldesc = tk.Label(window)
+
+def show():
+
+    cityName = entry.get()
+    api_key = "908a628a1815decea0de4a816a516291"
+    base_url = "https://api.openweathermap.org/data/2.5/weather?q="
+    org_url = base_url + cityName + "&appid=" + api_key
+    print(org_url)
+
+    x = requests.get(org_url)
+    y = x.json()
+
+    if y["cod"] == "404":
+
+        lblNotFound.config("city not found !")
+        lblNotFound.pack()
+
+    else:
+
+        z = y["main"]
+        t = z['temp']
+        temp = t - 273.15
+        lblTemp.config("temp = " + str(round(temp,2)) + " C")
+
+        pressure = z['pressure']
+        lblpressure.config("pressure = " + str(pressure) + " hpa")
+
+        humidity = z['humidity']
+        lblhumidity.config("humidity = " + str(humidity) + " %")
+
+        desc = y['weather']
+        descmain = desc[0]["description"]
+        lbldesc.config("description = " + descmain)
+
+        lblTemp.pack()
+        lblpressure.pack()
+        lblhumidity.pack()
+        lbldesc.pack()
+
+
+label = tk.Label(window,text="enter your city name : ")
+label.pack()
+
+entry = tk.Entry(window)
+entry.pack()
+
+btn1 = tk.Button(window,text = "show information",command=show)
+btn1.pack()
+
+window.mainloop()
